7e3d97b73621752c2980194a8d42dca007d57fb1
Sangin <ksiisk99@gmail.com>
2022-11-08 16:41:51 +0900

    feat: 교환 정보 엔티티 생성

---
+++ src/main/java/com/ay/exchange/exchange/entity/Exchange.java
@@ -0,0 +1,40 @@
+package com.ay.exchange.exchange.entity;
+
+import com.ay.exchange.board.entity.Board;
+import com.ay.exchange.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Exchange {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exchange_id")
+    private Long Id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Boolean type;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private String createdDate;
+}
