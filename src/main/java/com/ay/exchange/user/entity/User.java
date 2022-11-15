9eca8c4b332ed359a855c2280a7aa47f32703b2e
Sangin <ksiisk99@gmail.com>
2022-11-16 02:40:19 +0900

    feat: insert할때 select조회 제거

--- src/main/java/com/ay/exchange/user/entity/User.java
+++ src/main/java/com/ay/exchange/user/entity/User.java
@@ -1,21 +1,20 @@
 package com.ay.exchange.user.entity;
 
-import com.ay.exchange.board.entity.Board;
 import com.ay.exchange.common.entity.BaseEntity;
 import com.ay.exchange.exchange.entity.Exchange;
 import com.ay.exchange.user.entity.vo.Authority;
 import lombok.*;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
-public class User extends BaseEntity {
+public class User extends BaseEntity implements Persistable<String> {
     @Id
     @Column(name = "user_id", nullable = false)
     private String userId;
@@ -38,8 +37,18 @@ public class User extends BaseEntity {
     @Column(name = "profile_image")
     private String profileImage;
 
-    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY,orphanRemoval = true)
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, orphanRemoval = true)
     private Exchange exchange;
+
+    @Override
+    public String getId() {
+        return userId;
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreatedDate() == null;
+    }
 
 //    @OneToMany(mappedBy = "user")
 //    private List<Board> boards;
