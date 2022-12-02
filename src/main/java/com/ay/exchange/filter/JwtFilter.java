a950831f39b08da9c12cc526f0df3d12ffef1598
Sangin <ksiisk99@gmail.com>
2022-12-02 19:34:20 +0900

    fix: 특정 url에 cors허용을 위한 정규표현식 사용

--- src/main/java/com/ay/exchange/filter/JwtFilter.java
+++ src/main/java/com/ay/exchange/filter/JwtFilter.java
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
-    private final Set<String> passUris = Set.of("/user/sign-up", "/user/sign-in", "/user/sign-up/confirm/verification-code",
-            "/user/find-password/confirm/verification-code");
+    private final String passRegex="/user/sign-up|/user/sign-in|(/user/sign-up/confirm/verification-code)(\\?)?(email=[a-zA-z\\d-_.]*)|(/user/find-password/confirm/verification-code)(\\?)?(number=[\\d]*)";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -26,7 +26,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        if (passUris.contains(request.getRequestURI())) {
+        if (Pattern.matches(passRegex,request.getRequestURI())) {
             return true;
         }
         return false;
