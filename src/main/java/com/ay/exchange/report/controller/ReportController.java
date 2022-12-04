beff1c530530177b2fc7fa4452336f6d595a4ab1
Sangin <ksiisk99@gmail.com>
2022-12-05 01:51:36 +0900

    feat: report api 명세서에 예외사항 작성

--- src/main/java/com/ay/exchange/report/controller/ReportController.java
+++ src/main/java/com/ay/exchange/report/controller/ReportController.java
@@ -1,10 +1,14 @@
 package com.ay.exchange.report.controller;
 
+import com.ay.exchange.common.error.dto.ErrorDto;
 import com.ay.exchange.report.dto.request.ReportBoardRequest;
 import com.ay.exchange.report.dto.request.ReportCommentRequest;
 import com.ay.exchange.report.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +22,11 @@ public class ReportController {
 
     @Operation(summary = "게시글 신고",
             description = "게시글 신고",
-            parameters = {@Parameter(name = "token", description = "액세스 토큰")}
+            parameters = {@Parameter(name = "token", description = "액세스 토큰")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
+                    @ApiResponse(responseCode = "412", description = "이미 신고가 접수되었거나 오류로 신고에 실패하였습니다.", content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+            }
     )
     @PostMapping("/board")
     public Boolean reportBoard(
@@ -30,7 +38,11 @@ public class ReportController {
 
     @Operation(summary = "댓글 신고",
             description = "댓글 신고",
-            parameters = {@Parameter(name = "token", description = "액세스 토큰")}
+            parameters = {@Parameter(name = "token", description = "액세스 토큰")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
+                    @ApiResponse(responseCode = "412", description = "이미 신고가 접수되었거나 오류로 신고에 실패하였습니다.", content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+            }
     )
     @PostMapping("/comment")
     public Boolean reportComment(
