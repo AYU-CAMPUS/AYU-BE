daccaf9b610687b448ea3f1bcb34e3033ddc6c0f
Sangin <ksiisk99@gmail.com>
2022-12-25 23:24:48 +0900

    refactor: JwtException을 제외하고 에러로그 출력

--- src/main/java/com/ay/exchange/filter/JwtExceptionFilter.java
+++ src/main/java/com/ay/exchange/filter/JwtExceptionFilter.java
@@ -15,29 +15,30 @@ import java.io.IOException;
 
 
 @NoArgsConstructor
-public class JwtExceptionFilter extends OncePerRequestFilter{
+public class JwtExceptionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        try{
-            chain.doFilter(request,response);
-        }catch(Exception e){
+        try {
+            chain.doFilter(request, response);
+        } catch (JwtException e) {
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, e);
+        } catch (Exception e) {
             e.printStackTrace();
-            setErrorResponse(HttpStatus.UNAUTHORIZED,response,e);
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, e);
         }
     }
 
     private void setErrorResponse(HttpStatus status, HttpServletResponse response, Exception e) {
         response.setStatus(status.value());
         response.setContentType("application/json; charset=UTF-8");
-        System.out.println("JwtExceptionFIlter: "+e.getMessage());
         //common.util에 리팩토링
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
             response.getWriter()
                     .write(objectMapper.writeValueAsString(
-                            new ErrorDto(status.getReasonPhrase(),e.getMessage())));
+                            new ErrorDto(status.getReasonPhrase(), e.getMessage())));
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
