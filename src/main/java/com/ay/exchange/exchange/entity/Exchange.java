c803159592096479fbde4397e672d1999c9cc685
Sangin <ksiisk99@gmail.com>
2023-01-17 02:34:54 +0900

    fix: 같은 자료 중복 요청을 막기 위한 board_id 컬럼 Unique 제거

--- src/main/java/com/ay/exchange/exchange/entity/Exchange.java
+++ src/main/java/com/ay/exchange/exchange/entity/Exchange.java
@@ -25,7 +25,7 @@ import javax.persistence.*;
         uniqueConstraints = {
                 @UniqueConstraint(
                         name = "constraintExchange",
-                        columnNames = {"board_id", "requester_board_id", "email", "requester_email"}
+                        columnNames = {"requester_board_id", "email", "requester_email"}
                 )
         }
 )
