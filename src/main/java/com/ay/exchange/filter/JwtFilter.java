7c8d6d42250b122441f5df97c323c6e407b61083
Sangin <ksiisk99@gmail.com>
2023-02-19 00:58:53 +0900

    add: header url 로그추가

--- src/main/java/com/ay/exchange/filter/JwtFilter.java
+++ src/main/java/com/ay/exchange/filter/JwtFilter.java
@@ -58,7 +58,7 @@ public class JwtFilter extends OncePerRequestFilter {
         String formattedDate = seoulCurrentTime.format(formatter);
 
         log.info("{} {} {} => {} {}", request.getHeader(HttpHeaders.ORIGIN), formattedDate, request.getRequestURI(), getClientIP(request), request.getMethod());
-        log.info("host: {}, ", request.getHeader("host"));
+        log.info("url: {}, ", request.getHeader("url"));
         if (passUri.contains(request.getRequestURI())) {
             return true;
         }
