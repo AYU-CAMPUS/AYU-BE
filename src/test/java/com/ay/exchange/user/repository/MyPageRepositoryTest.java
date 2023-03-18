44592c11f2ef5058be4bc58763d5c9f8f5cd3805
Sangin <ksiisk99@gmail.com>
2023-03-19 00:16:28 +0900

    test: jpa 실패 테스트

--- src/test/java/com/ay/exchange/user/repository/MyPageRepositoryTest.java
+++ src/test/java/com/ay/exchange/user/repository/MyPageRepositoryTest.java
@@ -1,15 +1,28 @@
 package com.ay.exchange.user.repository;
 
+import com.ay.exchange.user.entity.User;
+import com.ay.exchange.user.entity.vo.Authority;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MyPageRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     void deploymentTest() {
-        assertEquals(true,false);
+        User user = User.builder()
+                .email("test@gmail.com")
+                .nickName("test")
+                .exchangeSuccessCount(0)
+                .desiredData("")
+                .authority(Authority.User)
+                .build();
+        userRepository.save(user);
+        assertEquals("test", userRepository.findById("test@gmail.com").get().getNickName());
     }
 }
