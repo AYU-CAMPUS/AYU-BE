8766c8d0f97d6b54feb2d3b1aeffb297df60b3d6
Sangin <ksiisk99@gmail.com>
2023-03-20 18:40:03 +0900

    test: 교환 요청 실패(내 자료가 존재하지 않음) 테스트

--- src/test/java/com/ay/exchange/exchange/service/ExchangeServiceTest.java
+++ src/test/java/com/ay/exchange/exchange/service/ExchangeServiceTest.java
@@ -1,14 +1,16 @@
 package com.ay.exchange.exchange.service;
 
+import com.ay.exchange.board.entity.Board;
+import com.ay.exchange.board.entity.vo.BoardCategory;
+import com.ay.exchange.board.entity.vo.Category;
+import com.ay.exchange.board.entity.vo.DepartmentType;
+import com.ay.exchange.board.entity.vo.FileType;
 import com.ay.exchange.board.repository.BoardRepository;
 import com.ay.exchange.exchange.dto.request.ExchangeRequest;
 import com.ay.exchange.exchange.exception.UnableExchangeException;
 import com.ay.exchange.user.entity.User;
 import com.ay.exchange.user.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -25,6 +27,9 @@ class ExchangeServiceTest {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    BoardRepository boardRepository;
 
     @BeforeAll
     void init() {
@@ -53,5 +58,39 @@ class ExchangeServiceTest {
         assertThrows(UnableExchangeException.class, () -> {
             exchangeService.requestExchange(exchangeRequest, "test@gmail.com");
         });
+    }
+
+    @Test
+    @DisplayName("내 자료가 존재하지 않음")
+    void 교환_요청_실패2() {
+        Board board = Board.builder()
+                .title("title")
+                .numberOfFilePages(1)
+                .filePath("filePath")
+                .originalFileName("fileName")
+                .approval(1)
+                .email("test2@gmail.com")
+                .boardCategory(BoardCategory.builder().
+                        category(Category.신학대학)
+                        .departmentType(DepartmentType.신학과)
+                        .fileType(FileType.중간고사)
+                        .gradeType("1")
+                        .subjectName("subject")
+                        .professorName("professor")
+                        .build())
+                .exchangeSuccessCount(0)
+                .build();
+        boardRepository.save(board);
+        ExchangeRequest exchangeRequest = new ExchangeRequest(board.getId(), board.getId()+1);
+
+        assertThrows(UnableExchangeException.class, () -> {
+            exchangeService.requestExchange(exchangeRequest, "test@gmail.com");
+        });
+    }
+
+    @AfterAll
+    void deleteEntity() {
+        userRepository.deleteById("test@gmail.com");
+        userRepository.deleteById("test2@gmail.com");
     }
 }
