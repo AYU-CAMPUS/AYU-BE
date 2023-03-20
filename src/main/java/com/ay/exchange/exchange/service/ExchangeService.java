4a29e9969b1cda96cdd94063640e720608ee8d2c
Sangin <ksiisk99@gmail.com>
2023-03-20 15:46:25 +0900

    fix: 교환 요청 시 내 자료 조회 파라미터로 token이 아닌 email로 전달

--- src/main/java/com/ay/exchange/exchange/service/ExchangeService.java
+++ src/main/java/com/ay/exchange/exchange/service/ExchangeService.java
@@ -5,7 +5,7 @@ import com.ay.exchange.exchange.dto.request.ExchangeRequest;
 import com.ay.exchange.exchange.dto.response.ExchangeResponse;
 import com.ay.exchange.exchange.exception.UnableExchangeException;
 import com.ay.exchange.exchange.repository.querydsl.ExchangeQueryRepository;
-import com.ay.exchange.jwt.JwtTokenProvider;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ExchangeService {
-    private final JwtTokenProvider jwtTokenProvider;
     private final ExchangeQueryRepository exchangeQueryRepository;
 
     public void requestExchange(ExchangeRequest exchangeRequest, String email) {
@@ -34,10 +33,10 @@ public class ExchangeService {
         }
     }
 
-    public ExchangeResponse getMyData(Integer page, String token) {
+    public ExchangeResponse getMyData(Integer page, String email) {
         PageRequest pageRequest = PageRequest.of(page > 0 ? (page - 1) : 0, 2,
                 Sort.by(Sort.Direction.DESC, "id"));
-        return exchangeQueryRepository.getMyData(pageRequest, jwtTokenProvider.getUserEmail(token));
+        return exchangeQueryRepository.getMyData(pageRequest, email);
     }
 
     public void existsExchangeCompletion(ExchangeRequest exchangeRequest) {
