cb0a141d48ba6eafeffc01f51c912a3172917df3
Sangin <ksiisk99@gmail.com>
2023-05-27 17:46:48 +0900

    fix: 페이징 크기 10으로 변경

--- src/main/java/com/ay/exchange/common/util/PagingGenerator.java
+++ src/main/java/com/ay/exchange/common/util/PagingGenerator.java
@@ -3,7 +3,7 @@ package com.ay.exchange.common.util;
 import org.springframework.data.domain.PageRequest;
 
 public class PagingGenerator {
-    private static final int SIZE = 2;
+    private static final int SIZE = 10;
 
     public static PageRequest getPageRequest(Integer page) {
         return PageRequest.of(page > 0 ? (page - 1) : 0, SIZE);
